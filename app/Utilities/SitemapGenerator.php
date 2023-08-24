226f7c2c016576731ac58960042d146dfcc0e315
Muhammad Asad Ibrahim <asad.ibrahim.944@gmail.com>
2023-08-24 06:10:05 -0700

    from offset 114

--- app/Utilities/SitemapGenerator.php
+++ app/Utilities/SitemapGenerator.php
@@ -145,10 +145,10 @@ class SitemapGenerator
         $iterations = ceil($totalQuestions / $chunkSize);
 
         // Initialize a variable to track the current offset
-        $offset = 5450109;
+        $offset = 5650113;
         $sitemap = Sitemap::create();
 
-        for ($i = 110; $i <= $iterations; $i++) {
+        for ($i = 114; $i <= $iterations; $i++) {
             $questions = $this->questionUrls($chunkSize, $offset);
 
             foreach ($questions as $url) {
