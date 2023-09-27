86e52bac6eef6cb2ccd6650d8166ff510b442e0d
Muhammad Asad Ibrahim <asad.ibrahim.944@gmail.com>
2023-09-27 08:39:02 -0700

    change time

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
         $schedule->call(function () {
             app(SitemapGenerator::class)->generate();
-        })->hourly();
+        })->everyFiveMinutes();
     }
 
     /**
