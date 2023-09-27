4a14880e6c4649f8caedffa4ecac72dc774aad25
Muhammad Asad Ibrahim <asad.ibrahim.944@gmail.com>
2023-09-27 09:26:36 -0700

    top eye doctor

--- app/Utilities/SitemapGenerator.php
+++ app/Utilities/SitemapGenerator.php
@@ -171,7 +171,7 @@ class SitemapGenerator
         $questions = DB::table('questions')
         ->select('topics.topic_name', 'questions.question')
         ->join('topics', 'questions.topic_id', '=', 'topics.id')
-        ->where('questions.question', '=','Top dentists') // Adjust the condition for starting ID
+        ->where('questions.question', '=','Top eye doctors') // Adjust the condition for starting ID
         ->get();
     
     return $questions;
