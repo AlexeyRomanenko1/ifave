6326672b159c3a5c5e38eacf6d09c6800fe27352
Muhammad Asad Ibrahim <asad.ibrahim.944@gmail.com>
2023-09-27 10:01:28 -0700

    top bridal shops

--- app/Utilities/SitemapGenerator.php
+++ app/Utilities/SitemapGenerator.php
@@ -109,7 +109,7 @@ class SitemapGenerator
             }
 
             // Write the sitemap to a file for each iteration
-            $sitemap->writeToFile(public_path("sitemap-questions-4.xml"));
+            $sitemap->writeToFile(public_path("sitemap-questions-5.xml"));
 
             // Move the offset for the next iteration
          //   $offset += $chunkSize;
@@ -171,7 +171,7 @@ class SitemapGenerator
         $questions = DB::table('questions')
         ->select('topics.topic_name', 'questions.question')
         ->join('topics', 'questions.topic_id', '=', 'topics.id')
-        ->where('questions.question', '=','Top bed and breakfast') // Adjust the condition for starting ID
+        ->where('questions.question', '=','Top bridal shops') // Adjust the condition for starting ID
         ->get();
     
     return $questions;
