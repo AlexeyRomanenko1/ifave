55fb7874322547cc101c4d08915c497d2544aa37
Muhammad Asad Ibrahim <asad.ibrahim.944@gmail.com>
2023-09-28 05:52:03 -0700

    coffee shop

--- app/Utilities/SitemapGenerator.php
+++ app/Utilities/SitemapGenerator.php
@@ -109,7 +109,7 @@ class SitemapGenerator
             }
 
             // Write the sitemap to a file for each iteration
-            $sitemap->writeToFile(public_path("sitemap-questions-76.xml"));
+            $sitemap->writeToFile(public_path("sitemap-questions-77.xml"));
 
             // Move the offset for the next iteration
          //   $offset += $chunkSize;
@@ -171,7 +171,7 @@ class SitemapGenerator
         $questions = DB::table('questions')
         ->select('topics.topic_name', 'questions.question')
         ->join('topics', 'questions.topic_id', '=', 'topics.id')
-        ->where('questions.question', '=','Top garden centers') // Adjust the condition for starting ID
+        ->where('questions.question', '=','Top cafes (coffee shops)') // Adjust the condition for starting ID
         ->get();
     
     return $questions;
