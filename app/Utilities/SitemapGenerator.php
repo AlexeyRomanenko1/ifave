1093b3c1ec42c5abd8da4a2cf9e79173f67f1e75
Muhammad Asad Ibrahim <asad.ibrahim.944@gmail.com>
2023-09-28 08:59:45 -0700

    mayors

--- app/Utilities/SitemapGenerator.php
+++ app/Utilities/SitemapGenerator.php
@@ -109,7 +109,7 @@ class SitemapGenerator
             }
 
             // Write the sitemap to a file for each iteration
-            $sitemap->writeToFile(public_path("sitemap-questions-92.xml"));
+            $sitemap->writeToFile(public_path("sitemap-questions-93.xml"));
 
             // Move the offset for the next iteration
          //   $offset += $chunkSize;
@@ -171,7 +171,7 @@ class SitemapGenerator
         $questions = DB::table('questions')
         ->select('topics.topic_name', 'questions.question')
         ->join('topics', 'questions.topic_id', '=', 'topics.id')
-        ->where('questions.question', '=','Top music performers') // Adjust the condition for starting ID
+        ->where('questions.question', '=','Top city mayors') // Adjust the condition for starting ID
         ->get();
     
     return $questions;
