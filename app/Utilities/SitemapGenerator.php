8d08922f1f82e0b859702a2a44b00baa2b200374
Muhammad Asad Ibrahim <asad.ibrahim.944@gmail.com>
2023-09-28 01:55:31 -0700

    top towing company

--- app/Utilities/SitemapGenerator.php
+++ app/Utilities/SitemapGenerator.php
@@ -109,7 +109,7 @@ class SitemapGenerator
             }
 
             // Write the sitemap to a file for each iteration
-            $sitemap->writeToFile(public_path("sitemap-questions-48.xml"));
+            $sitemap->writeToFile(public_path("sitemap-questions-49.xml"));
 
             // Move the offset for the next iteration
          //   $offset += $chunkSize;
@@ -171,7 +171,7 @@ class SitemapGenerator
         $questions = DB::table('questions')
         ->select('topics.topic_name', 'questions.question')
         ->join('topics', 'questions.topic_id', '=', 'topics.id')
-        ->where('questions.question', '=','Top security systems') // Adjust the condition for starting ID
+        ->where('questions.question', '=','Top towing services') // Adjust the condition for starting ID
         ->get();
     
     return $questions;
