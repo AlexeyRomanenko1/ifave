a8d0bcb11623973177587b292c13a20aaf41b0db
Muhammad Asad Ibrahim <asad.ibrahim.944@gmail.com>
2023-09-28 01:44:53 -0700

    top moving companies

--- app/Utilities/SitemapGenerator.php
+++ app/Utilities/SitemapGenerator.php
@@ -109,7 +109,7 @@ class SitemapGenerator
             }
 
             // Write the sitemap to a file for each iteration
-            $sitemap->writeToFile(public_path("sitemap-questions-45.xml"));
+            $sitemap->writeToFile(public_path("sitemap-questions-44.xml"));
 
             // Move the offset for the next iteration
          //   $offset += $chunkSize;
@@ -171,7 +171,7 @@ class SitemapGenerator
         $questions = DB::table('questions')
         ->select('topics.topic_name', 'questions.question')
         ->join('topics', 'questions.topic_id', '=', 'topics.id')
-        ->where('questions.question', '=','Top plumbers') // Adjust the condition for starting ID
+        ->where('questions.question', '=','Top moving companies') // Adjust the condition for starting ID
         ->get();
     
     return $questions;
