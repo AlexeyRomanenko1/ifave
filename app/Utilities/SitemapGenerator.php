3e825597de5cd5d96de79a2f474b9992689b2b11
Muhammad Asad Ibrahim <asad.ibrahim.944@gmail.com>
2023-10-03 02:03:39 -0700

    new reporter

--- app/Utilities/SitemapGenerator.php
+++ app/Utilities/SitemapGenerator.php
@@ -109,7 +109,7 @@ class SitemapGenerator
             }
 
             // Write the sitemap to a file for each iteration
-            $sitemap->writeToFile(public_path("sitemap-questions-112.xml"));
+            $sitemap->writeToFile(public_path("sitemap-questions-113.xml"));
 
             // Move the offset for the next iteration
          //   $offset += $chunkSize;
@@ -171,7 +171,7 @@ class SitemapGenerator
         $questions = DB::table('questions')
         ->select('topics.topic_name', 'questions.question')
         ->join('topics', 'questions.topic_id', '=', 'topics.id')
-        ->where('questions.question', '=','Top doctors') // Adjust the condition for starting ID
+        ->where('questions.question', '=','Top news reporters') // Adjust the condition for starting ID
         ->get();
     
     return $questions;
