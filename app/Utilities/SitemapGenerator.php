80568dd2d4db30e12608d600b6e14308f7be3d60
Muhammad Asad Ibrahim <asad.ibrahim.944@gmail.com>
2023-10-03 06:57:32 -0700

    paying occupations

--- app/Utilities/SitemapGenerator.php
+++ app/Utilities/SitemapGenerator.php
@@ -109,7 +109,7 @@ class SitemapGenerator
             }
 
             // Write the sitemap to a file for each iteration
-            $sitemap->writeToFile(public_path("sitemap-questions-153.xml"));
+            $sitemap->writeToFile(public_path("sitemap-questions-154.xml"));
 
             // Move the offset for the next iteration
          //   $offset += $chunkSize;
@@ -171,7 +171,7 @@ class SitemapGenerator
         $questions = DB::table('questions')
         ->select('topics.topic_name', 'questions.question')
         ->join('topics', 'questions.topic_id', '=', 'topics.id')
-        ->where('questions.question', '=','Soul of the city (town)') // Adjust the condition for starting ID
+        ->where('questions.question', '=','Top paying occupations') // Adjust the condition for starting ID
         ->get();
     
     return $questions;
