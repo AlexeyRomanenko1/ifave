6d943d3da3ebbc4e952a5c01e9480d1fdb3dd3b8
Muhammad Asad Ibrahim <asad.ibrahim.944@gmail.com>
2023-10-03 02:22:10 -0700

    savouir

--- app/Utilities/SitemapGenerator.php
+++ app/Utilities/SitemapGenerator.php
@@ -109,7 +109,7 @@ class SitemapGenerator
             }
 
             // Write the sitemap to a file for each iteration
-            $sitemap->writeToFile(public_path("sitemap-questions-119.xml"));
+            $sitemap->writeToFile(public_path("sitemap-questions-120.xml"));
 
             // Move the offset for the next iteration
          //   $offset += $chunkSize;
@@ -171,7 +171,7 @@ class SitemapGenerator
         $questions = DB::table('questions')
         ->select('topics.topic_name', 'questions.question')
         ->join('topics', 'questions.topic_id', '=', 'topics.id')
-        ->where('questions.question', '=','Top gay places') // Adjust the condition for starting ID
+        ->where('questions.question', '=','Top souvenir shops') // Adjust the condition for starting ID
         ->get();
     
     return $questions;
