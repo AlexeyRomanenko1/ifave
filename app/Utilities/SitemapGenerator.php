7654f92834edbee09848be2448ebbd37f0495fac
Muhammad Asad Ibrahim <asad.ibrahim.944@gmail.com>
2023-10-03 13:22:43 -0700

    the world

--- app/Utilities/SitemapGenerator.php
+++ app/Utilities/SitemapGenerator.php
@@ -109,7 +109,7 @@ class SitemapGenerator
             }
 
             // Write the sitemap to a file for each iteration
-            $sitemap->writeToFile(public_path("sitemap-questions-179.xml"));
+            $sitemap->writeToFile(public_path("sitemap-questions-19.xml"));
 
             // Move the offset for the next iteration
          //   $offset += $chunkSize;
@@ -171,7 +171,7 @@ class SitemapGenerator
         $questions = DB::table('questions')
         ->select('topics.topic_name', 'questions.question')
         ->join('topics', 'questions.topic_id', '=', 'topics.id')
-        ->where('questions.question', '=','Top construction and renovation companies') // Adjust the condition for starting ID
+        ->where('questions.topic_id', '=',1) // Adjust the condition for starting ID
         ->get();
     
     return $questions;
