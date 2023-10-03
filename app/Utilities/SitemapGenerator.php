421d65cfa8437b22371bae4f854c93ed0d818afe
Muhammad Asad Ibrahim <asad.ibrahim.944@gmail.com>
2023-10-03 12:46:12 -0700

    fun things to do

--- app/Utilities/SitemapGenerator.php
+++ app/Utilities/SitemapGenerator.php
@@ -109,7 +109,7 @@ class SitemapGenerator
             }
 
             // Write the sitemap to a file for each iteration
-            $sitemap->writeToFile(public_path("sitemap-questions-168.xml"));
+            $sitemap->writeToFile(public_path("sitemap-questions-169.xml"));
 
             // Move the offset for the next iteration
          //   $offset += $chunkSize;
@@ -171,7 +171,7 @@ class SitemapGenerator
         $questions = DB::table('questions')
         ->select('topics.topic_name', 'questions.question')
         ->join('topics', 'questions.topic_id', '=', 'topics.id')
-        ->where('questions.question', '=','Top travel tips for tourists') // Adjust the condition for starting ID
+        ->where('questions.question', '=','Top fun things to do') // Adjust the condition for starting ID
         ->get();
     
     return $questions;
